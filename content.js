1253660d359d4f26d97ea0e40618423d39714b76
puliandc <lujiaxian@gmail.com>
2023-06-01 18:49:21 +1000

    功能修改为点击图标复制到剪贴板。

--- content.js
+++ content.js
@@ -1,9 +1,6 @@
 var targetElement = document.querySelector('#mainContent > div > div.q-box.qu-borderAll.qu-borderRadius--small.qu-borderColor--raised.qu-boxShadow--small.qu-bg--raised:first-child > div.q-box.qu-pt--medium.qu-px--medium.qu-pb--tiny:first-child');
-var parentElement = targetElement.parentElement;
-var childElements = parentElement.children;
+var htmlString = targetElement.outerHTML;
 
-for (var i = 0; i < childElements.length; i++) {
-    if (childElements[i] !== targetElement) {
-        childElements[i].style.display = 'none';
-    }
-}
+chrome.runtime.sendMessage({action: "copy", html: htmlString});
+
+console.log("Content script has been loaded and executed.");
