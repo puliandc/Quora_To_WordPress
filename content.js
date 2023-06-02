621c01a15889acadbf619b19cceb8178469265b4
puliandc <lujiaxian@gmail.com>
2023-06-02 22:31:46 +1000

    可以正确提取Quora的提问和回答了。明天处理如何发布到Wrodpress的问题。

--- content.js
+++ content.js
@@ -1,17 +1,29 @@
 // content.js
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+  function(request, sender, sendResponse) {
       if (request.message === "clicked_browser_action") {
-        let titleElement = document.querySelector('.puppeteer_test_question_title');
-        let title = titleElement ? titleElement.innerText : ''; 
-  
-        let htmlString = document.querySelector('.q-text').innerHTML;
-        let parser = new DOMParser();
-        let htmlDoc = parser.parseFromString(htmlString, 'text/html');
-        let textContent = Array.from(htmlDoc.querySelectorAll('p')).map(p => p.textContent).join('\n');
-  
-        sendResponse({title: title, text: textContent});
+          let titleElement = document.querySelector('.puppeteer_test_question_title');
+          let title = titleElement ? titleElement.innerText : '';
+
+          // query for the first div element with class 'q-text'
+          let divElement = document.querySelector("div[class='q-text']");
+
+          if (divElement) {
+              // query for all the p elements within the div
+              let pElements = divElement.querySelectorAll('p');
+
+              // create an array of the text contents of these elements
+              let textContents = Array.from(pElements).map(p => p.textContent);
+
+              // join all the text contents together with a newline character
+              let text = textContents.join('\n');
+
+              sendResponse({title: title, text: text});
+          } else {
+              sendResponse({title: title, text: ""});
+          }
       }
-    }
-  );
-  
+      // This is required for Chrome to call the sendResponse callback.
+      return true;  
+  }
+);
