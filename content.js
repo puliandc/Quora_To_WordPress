2052e89d4c16a6cd87aae176e9f52a7480ef88f1
puliandc <lujiaxian@gmail.com>
2023-06-03 21:21:05 +1000

    将querySelectorAll方法的参数改为'p, li'，以便同时选择<p>和<li>元素。 然后，我在每个元素的文本内容后面添加了一个换行符。 最后，我使用join('')方法将所有的文本内容连接在一起，而不是使用join('\n')。 这样，每个段落或列表项后面都会有一个换行符，而不会有多余的换行符。

--- content.js
+++ content.js
@@ -1,29 +1,29 @@
 // content.js
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-      if (request.message === "clicked_browser_action") {
-          let titleElement = document.querySelector('.puppeteer_test_question_title');
-          let title = titleElement ? titleElement.innerText : '';
+    function(request, sender, sendResponse) {
+        if (request.message === "clicked_browser_action") {
+            let titleElement = document.querySelector('.puppeteer_test_question_title');
+            let title = titleElement ? titleElement.innerText : ''; 
 
-          // query for the first div element with class 'q-text'
-          let divElement = document.querySelector("div[class='q-text']");
+            // query for the first div element with class 'q-text'
+            let divElement = document.querySelector("div[class='q-text']");
 
-          if (divElement) {
-              // query for all the p elements within the div
-              let pElements = divElement.querySelectorAll('p');
+            if (divElement) {
+                // select all 'p' and 'li' elements within the div
+                let textElements = divElement.querySelectorAll('p, li');
 
-              // create an array of the text contents of these elements
-              let textContents = Array.from(pElements).map(p => p.textContent);
+                // create an array of the text contents of these elements, each followed by a newline character
+                let textContents = Array.from(textElements).map(el => el.textContent + '\n');
 
-              // join all the text contents together with a newline character
-              let text = textContents.join('\n');
+                // join all the text contents together
+                let text = textContents.join('');
 
-              sendResponse({title: title, text: text});
-          } else {
-              sendResponse({title: title, text: ""});
-          }
-      }
-      // This is required for Chrome to call the sendResponse callback.
-      return true;  
-  }
+                sendResponse({title: title, text: text});
+            } else {
+                sendResponse({title: title, text: ""});
+            }
+        }
+        // This is required for Chrome to call the sendResponse callback.
+        return true;  
+    }
 );
